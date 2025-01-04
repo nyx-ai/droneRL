a02f2f5b42c4c618075fb30606fc2227cd94214e
Martin Muller <martin.muller.me@gmail.com>
2025-01-04 14:48:25 +0300

    improve benchmarking, add scan train loop

--- jax/train.py
+++ jax/train.py
@@ -5,6 +5,8 @@ import jax.random
 from tqdm import trange
 from jax.experimental.compilation_cache import compilation_cache as cc
 from timeit import default_timer as timer
+from collections import defaultdict
+import statistics
 
 from env.env import DroneEnvParams, DeliveryDrones
 from env.constants import Action
@@ -26,6 +28,8 @@ def train():
     hidden_layers = (32, 32)
     n_drones = 3
     grid_size = 8
+    # n_drones = 1024
+    # grid_size = 185
 
     env_params = DroneEnvParams(n_drones=n_drones, grid_size=grid_size)
     ag_params = DQNAgentParams(hidden_layers=hidden_layers)
@@ -50,6 +54,79 @@ def train():
     exp = {'obs': obs, 'actions': actions[0], 'rewards': rewards[0], 'next_obs': next_obs, 'dones': dones[0]}
     bstate = buffer.init(exp)
 
+    ##################
+    # JAX scan loop
+    ##################
+    def _update(carry, step):
+        rng, state, obs, ag_state, bstate = carry
+
+        rng, key = jax.random.split(rng)
+        # generate random actions for all drones
+        actions = jax.random.randint(key, (env_params.n_drones,), minval=0, maxval=Action.num_actions())
+
+        # run action for DQN agent
+        dqn_action = dqn_agent.act(key, obs, ag_state)
+        actions = actions.at[0].set(dqn_action)
+
+        # perform actions in env
+        state, rewards, dones = env.step(key, state, actions, env_params)
+
+        next_obs = env.get_obs(state, env_params)
+        next_obs = next_obs[0].ravel()
+
+        # add to buffer
+        exp = {'obs': obs, 'actions': actions[0], 'rewards': rewards[0], 'next_obs': next_obs, 'dones': dones[0]}
+        bstate = buffer.add(bstate, exp)
+
+        # train step
+        def train_if_can_sample(args):
+            ag_state, bstate, key = args
+            batch, key = buffer.sample(key, bstate)
+            trained_state, loss = dqn_agent.train_step(
+                ag_state,
+                batch['obs'],
+                batch['actions'],
+                batch['rewards'],
+                batch['next_obs'],
+                batch['dones'],
+                ag_params)
+            return trained_state
+
+        ag_state = jax.lax.cond(
+            buffer.can_sample(bstate),
+            train_if_can_sample,
+            lambda x: x[0],
+            (ag_state, bstate, key)
+        )
+
+        # update target network
+        ag_state = jax.lax.cond(
+            step % ag_params.target_update_interval == 0,
+            lambda x: dqn_agent.update_target(x, ag_params),
+            lambda x: x,
+            ag_state
+        )
+
+        # update epsilon
+        ag_state = jax.lax.cond(
+            dones[0],
+            lambda x: dqn_agent.update_epsilon(x, ag_params),
+            lambda x: x,
+            ag_state
+        )
+
+        return (rng, state, next_obs, ag_state, bstate), None
+
+    # ts = timer()
+    # logger.info('Starting scan...')
+    # init_carry = (rng, state, obs, ag_state, bstate)
+    # final_carry, _ = jax.lax.scan(_update, init_carry, jnp.arange(num_steps))
+    # final_carry[0].block_until_ready()
+    # logger.info(f'... full scan took {timer()-ts:.1f}s/1k steps')
+
+    ##################
+    # Python for-looop
+    ##################
     # jit functions
     # do_jit = False
     do_jit = True
@@ -59,7 +136,12 @@ def train():
     get_obs_jit = jax.jit(env.get_obs, static_argnums=(1,)) if do_jit else env.get_obs
     buffer_add_jit = jax.jit(buffer.add) if do_jit else buffer.add
     buffer_sample_jit = jax.jit(buffer.sample) if do_jit else buffer.sample
-
+    update_target_jit = jax.jit(dqn_agent.update_target, static_argnums=(1,)) if do_jit else dqn_agent.update_target
+    update_eps_jit = jax.jit(dqn_agent.update_epsilon, static_argnums=(1,)) if do_jit else dqn_agent.update_epsilon
+    timing = defaultdict(list)
+    step_timing = []
+    do_timing = True
+    skip_timing = 100
 
     for step in trange(num_steps):
         ts_all = timer()
@@ -68,34 +150,38 @@ def train():
         # generate random actions for all drones
         actions = jax.random.randint(key, (env_params.n_drones,), minval=0, maxval=Action.num_actions())
 
-        ts = timer()
         # run action for DQN agent
-        dqn_action = act_jit(key, obs, ag_state)
-        actions = actions.at[0].set(dqn_action)
-        logger.info(f'{1000*(timer()-ts):.2f}ms agent act')
-
         ts = timer()
+        dqn_action = act_jit(key, obs, ag_state)
+        if do_timing and step > skip_timing:
+            timing['agent_act'].append(timer() - ts)
+        actions = actions.at[0].set(dqn_action)
+
         # perform actions in env
+        ts = timer()
         state, rewards, dones = step_jit(key, state, actions, env_params)
-        logger.info(f'{1000*(timer()-ts):.2f}ms env step')
+        if do_timing and step > skip_timing:
+            timing['env_step'].append(timer() - ts)
 
         ts = timer()
         next_obs = get_obs_jit(state, env_params)
+        if do_timing and step > skip_timing:
+            timing['get_obs'].append(timer() - ts)
         next_obs = next_obs[0].ravel()
-        logger.info(f'{1000*(timer()-ts):.2f}ms env get obs')
 
         # add to buffer
-        ts = timer()
         exp = {'obs': obs, 'actions': actions[0], 'rewards': rewards[0], 'next_obs': next_obs, 'dones': dones[0]}
+        ts = timer()
         bstate = buffer_add_jit(bstate, exp)
-
-        logger.info(f'{1000*(timer()-ts):.2f}ms buffer add')
+        if do_timing and step > skip_timing:
+            timing['buffer_add'].append(timer() - ts)
 
         # train step
         if buffer.can_sample(bstate):
             ts = timer()
             batch, key = buffer_sample_jit(key, bstate)
-            logger.info(f'{1000*(timer()-ts):.2f}ms buffer sample')
+            if do_timing and step > skip_timing:
+                timing['buffer_sample'].append(timer() - ts)
             ts = timer()
             ag_state, loss = train_step_jit(
                     ag_state,
@@ -105,22 +191,38 @@ def train():
                     batch['next_obs'],
                     batch['dones'],
                     ag_params)
-            logger.info(f'{1000*(timer()-ts):.2f}ms train step')
+            if do_timing and step > skip_timing:
+                timing['train_step'].append(timer() - ts)
 
-        ts = timer()
         # update target network
         if step % ag_params.target_update_interval == 0:
-            ag_state = dqn_agent.update_target(ag_state, ag_params)
-        logger.info(f'{1000*(timer()-ts):.2f}ms update network')
+            ts = timer()
+            ag_state = update_target_jit(ag_state, ag_params)
+            if do_timing and step > skip_timing:
+                timing['update_network'].append(timer() - ts)
 
         # update eps
-        ts = timer()
         if dones[0]:
-            ag_state = dqn_agent.update_epsilon(ag_state, ag_params)
-        logger.info(f'{1000*(timer()-ts):.2f}ms update eps')
+            ts = timer()
+            ag_state = update_eps_jit(ag_state, ag_params)
+            if do_timing and step > skip_timing:
+                timing['update_eps'].append(timer() - ts)
 
         obs = next_obs
-        logger.info(f'... full step {1000*(timer()-ts_all):.1f}s/1k steps')
+        if do_timing and step > skip_timing:
+            step_timing.append(timer() - ts_all)
+
+    def _stat(mylist, factor=1000):
+        return factor * statistics.mean(mylist), factor * statistics.stdev(mylist)
+    step_mean, step_stdev = _stat(step_timing)
+    stats = {k: _stat(v) for k, v in timing.items()}
+    max_key_length = max(len(k) for k in stats.keys())
+    spacing = 9 * ' '
+    stats_str = f'\nFull step{spacing} {step_mean:.3f} ± {step_stdev:>6.3f} s/k steps\n'
+    for k, (mean, std) in stats.items():
+        stats_str += f'  - {k:<{max_key_length}} {mean:.3f} ± {std:>6.3f} s/k steps ({100*mean/step_mean:>5.2f}%)\n'
+    logger.info(stats_str)
+    __import__('pdb').set_trace()
 
 
 if __name__ == "__main__":
