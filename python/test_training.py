d488d0a8b5983c3d81772b990bb4dddcfe35dccb
Florian Laurent <florian.laurent@gmail.com>
2025-02-10 10:52:46 +0100

    disabling some training tests for now

--- python/test_training.py
+++ python/test_training.py
@@ -1,30 +1,21 @@
 import statistics
-import pytest
+from dataclasses import dataclass
+import numpy as np
 from tqdm import tqdm
+from tabulate import tabulate
+import matplotlib.pyplot as plt
 from env.env import DeliveryDrones
 from env.wrappers import WindowedGridView
 from agents.dqn import DQNAgent, DenseQNetworkFactory
 from agents.random import RandomAgent
+from pprint import pprint
 
 
-@pytest.fixture
-def env():
-    return WindowedGridView(DeliveryDrones(), radius=3)
-
-
-@pytest.fixture
-def base_config():
-    return {
-        'n_steps': 1_000,
-        'n_drones': 4,
-        'pickup_reward': 0.5
-    }
-
-
-def train(env, config, n_drones, n_steps):
+def benchmark_implementation(env, config, n_drones, n_steps):
     config_params = {**config, 'n_drones': n_drones, 'pickup_reward': 0.5}
     env.env_params.update(config_params)
     states = env.reset()
+    print(env.render())
 
     rewards_log = {}
     agents = {drone.index: RandomAgent(env) for drone in env.drones}
@@ -45,29 +36,53 @@ def train(env, config, n_drones, n_steps):
         target_update_interval=4
     )
 
-    for _ in tqdm(range(n_steps), leave=True):
+    for i in tqdm(range(n_steps), leave=True):
         actions = {key: agent.act(states[key]) for key, agent in agents.items()}
-        next_states, rewards, dones, info, _ = env.step(actions)
+        step = env.step(actions)
+
+        next_states, rewards, dones, info, _ = step
         for key, agent in agents.items():
             agent.learn(states[key], actions[key], rewards[key], next_states[key], dones[key])
             rewards_log[key].append(rewards[key])
         states = next_states
 
-    return {
-        'reward_trained': statistics.mean(rewards_log[0][-100:]),
-        'reward_untrained': statistics.mean(rewards_log[1][-100:])
+    reward_trained = statistics.mean(rewards_log[0][-100:])
+    reward_untrained = statistics.mean(rewards_log[1][-100:])
+
+    result = {
+        'n_drones': n_drones,
+        'grid_size': f"{env.side_size}x{env.side_size}",
+        'reward_trained': reward_trained,
+        'reward_untrained': reward_untrained
     }
+    pprint(result)
 
 
-def test_drones_only(env, base_config):
+def test_small_grid_drones_only():
+    env = WindowedGridView(DeliveryDrones(), radius=3)
     config = {'packets_factor': 0, 'dropzones_factor': 0, 'stations_factor': 0, 'skyscrapers_factor': 0}
-    train(env, config, base_config['n_drones'], base_config['n_steps'])
+    n_steps = 1000
+    n_drones = 4
+    benchmark_implementation(env, config, n_drones, n_steps)
 
 
-def test_default_environment(env, base_config):
-    train(env, {}, base_config['n_drones'], base_config['n_steps'])
+# def test_small_grid_default():
+#     env = WindowedGridView(DeliveryDrones(), radius=3)
+#     config = {}
+#     n_steps = 1000
+#     n_drones = 4
+#     benchmark_implementation(env, config, n_drones, n_steps)
 
 
-def test_high_density(env, base_config):
-    config = {'packets_factor': 4, 'dropzones_factor': 4, 'stations_factor': 4, 'skyscrapers_factor': 4}
-    train(env, config, base_config['n_drones'], base_config['n_steps'])
+# def test_small_grid_high_density():
+#     env = WindowedGridView(DeliveryDrones(), radius=3)
+#     config = {'packets_factor': 4, 'dropzones_factor': 4, 'stations_factor': 4, 'skyscrapers_factor': 4}
+#     n_steps = 1000
+#     n_drones = 4
+#     benchmark_implementation(env, config, n_drones, n_steps)
+
+
+if __name__ == '__main__':
+    test_small_grid_drones_only()
+    # test_small_grid_default()
+    # test_small_grid_high_density()
