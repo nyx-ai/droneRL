a40651b3928128eab439005fc3d4f17939116d6e
Florian Laurent <florian.laurent@gmail.com>
2025-02-16 02:44:33 +0100

    refactored wrappers

--- torch_impl/env/wrappers.py
+++ torch_impl/env/wrappers.py
@@ -3,55 +3,13 @@ import gym.spaces as spaces
 import gym
 
 
-class GridView(gym.ObservationWrapper):
+class BaseGridView(gym.ObservationWrapper):
     def __init__(self, env):
         super().__init__(env, new_step_api=True)
-        self.observation_space = spaces.Box(
-            low=0, high=1, shape=(self.side_size, self.side_size, 6), dtype=float
-        )
 
-    def observation(self, _):
-        grid = np.zeros((self.env.side_size, self.env.side_size, 6), dtype=np.float32)
-        print(grid.shape)
-
-        for (y, x), drone in self.env.drones.items():
-            grid[y, x, 0] = 1
-            if drone.packet is not None:
-                grid[y, x, 1] = 1
-            grid[y, x, 4] = drone.charge / 100
-
-        for (y, x) in self.env.packets.keys():
-            grid[y, x, 1] = 1
-
-        for (y, x) in self.env.dropzones.keys():
-            grid[y, x, 2] = 1
-
-        for (y, x) in self.env.stations.keys():
-            grid[y, x, 3] = 1
-
-        for (y, x) in self.env.skyscrapers.keys():
-            grid[y, x, 5] = 1
-
-        states = {}
-        for (y, x), drone in self.env.drones.items():
-            states[drone.index] = grid.copy()
-        return states
-
-
-class WindowedGridView(gym.ObservationWrapper):
-    def __init__(self, env, radius):
-        super().__init__(env, new_step_api=True)
-        self.radius = radius
-        assert radius > 0, "Radius should be strictly positive"
-        self.observation_space = spaces.Box(
-            low=0, high=1, shape=(self.radius * 2 + 1, self.radius * 2 + 1, 6), dtype=float
-        )
-
-    def observation(self, _):
-        # Compute full padded grid size
+    def _create_base_grid(self):
         grid = np.zeros((self.env.side_size, self.env.side_size, 6), dtype=np.float32)
 
-        # Mark drones, packets, dropzones, stations, obstacles
         for (y, x), drone in self.env.drones.items():
             grid[y, x, 0] = 1
             if drone.packet:
@@ -70,19 +28,40 @@ class WindowedGridView(gym.ObservationWrapper):
         for (y, x) in self.env.skyscrapers.keys():
             grid[y, x, 5] = 1
 
-        # Larger grid to include parts outside of the walls
-        padded_size = self.env.side_size + 2 * self.radius
-        padded_shape = (padded_size, padded_size, 6)
-        padded_grid = np.zeros(padded_shape)
-        padded_grid[:, :, 5] = 1
+        return grid
 
-        # Reference to the actual grid part excluding padding
-        grid = padded_grid[self.radius:-self.radius, self.radius:-self.radius] = grid
+
+class GridView(BaseGridView):
+    def __init__(self, env):
+        super().__init__(env)
+        self.observation_space = spaces.Box(
+            low=0, high=1, shape=(self.side_size, self.side_size, 6), dtype=float
+        )
+
+    def observation(self, _):
+        grid = self._create_base_grid()
+        return {drone.index: grid.copy() for (_, _), drone in self.env.drones.items()}
+
+
+class WindowedGridView(BaseGridView):
+    def __init__(self, env, radius):
+        super().__init__(env)
+        self.radius = radius
+        assert radius > 0, "Radius should be strictly positive"
+        self.observation_space = spaces.Box(
+            low=0, high=1, shape=(self.radius * 2 + 1, self.radius * 2 + 1, 6), dtype=float
+        )
+
+    def observation(self, _):
+        grid = self._create_base_grid()
+        
+        padded_size = self.env.side_size + 2 * self.radius
+        padded_grid = np.zeros((padded_size, padded_size, 6))
+        padded_grid[:, :, 5] = 1
+        padded_grid[self.radius:-self.radius, self.radius:-self.radius] = grid
 
         states = {}
-        # Extract windowed views for each drone
         for (y, x), drone in self.env.drones.items():
-            # Calculate absolute positions with padding offset
             top_left_y = y + self.radius
             top_left_x = x + self.radius
             states[drone.index] = padded_grid[
